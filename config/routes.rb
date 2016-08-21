5e3cdf037e5f64d0ff4352ae30bf921b47eb4544
David Vezzani <david_vezzani@yahoo.com>
2016-08-21 00:54:18 -0700

    adds welcome page and protects users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resources :users
 
   #get 'session/create'
@@ -12,7 +14,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
