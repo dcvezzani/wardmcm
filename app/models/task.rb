8b9d3d602aae7d5563d9f341b03c768805ced63e
David Vezzani <david_vezzani@yahoo.com>
2016-08-21 17:25:32 -0700

    fixes bad polymorphic references

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,5 @@ class Task < ActiveRecord::Base
   belongs_to :requester, foreign_key: :assigned_by_id, class_name: 'User'
   has_many :assignments
   has_many :owners, through: :assignments, foreign_key: :assigned_to_id
-  has_many :notes, as: :notable, polymorphic: true
+  has_many :notes, as: :notable
 end
