985d1455ad989b225c3bc0fe0f17ab98c008ee2d
David Vezzani <david_vezzani@yahoo.com>
2016-09-21 11:17:57 -0700

    adds recipe model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822003318) do
+ActiveRecord::Schema.define(version: 20160921181705) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer  "task_id",        limit: 4
@@ -59,6 +59,13 @@ ActiveRecord::Schema.define(version: 20160822003318) do
     t.integer  "author_id",    limit: 4
     t.string   "title",        limit: 255
     t.text     "body",         limit: 65535
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string   "name",         limit: 255
+    t.text     "instructions", limit: 65535
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
   end
