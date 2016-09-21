9bfa219c0866883535acd41f8f4aca47df8d7e11
David Vezzani <david_vezzani@yahoo.com>
2016-09-21 12:09:46 -0700

    add service to retrieve recipes, json format

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,9 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = if params[:keywords]
+                 Recipe.where('upper(name) like upper(?)',"%#{params[:keywords]}%")
+               else
+                 []
+               end    
   end
 end
